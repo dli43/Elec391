ea24270facde471a137fcc8c2210cc6275b81108
Graham <gsmith13@student.ubc.ca>
2025-05-16 17:19:51 -0700

    plotting stuff

--- arduino.ino
+++ arduino.ino
@@ -7,21 +7,21 @@ void setup() {
 
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("Started");
+  //Serial.println("Started");
   
   IMU.begin();
 
   if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
+    //Serial.println("Failed to initialize IMU!");
     while (1);
   }
 
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println(" Hz");
-  Serial.println();
-  Serial.println("Acceleration in G's");
-  Serial.println("X\tY\tZ");
+  // Serial.print("Accelerometer sample rate = ");
+  // Serial.print(IMU.accelerationSampleRate());
+  // Serial.println(" Hz");
+  // Serial.println();
+  // Serial.println("Acceleration in G's");
+  // Serial.println("X\tY\tZ");
 
   theta = 0; //measured angle, initialize to zero
 
@@ -42,35 +42,37 @@ void loop() {
     IMU.readAcceleration(ax, ay, az);
 
     //for debugging
-    Serial.print("ax = ");
-    Serial.println(ax);
-    Serial.print("ay = ");
-    Serial.println(ay);
-    Serial.print("az = ");
-    Serial.println(az);
+    // Serial.print("ax = ");
+    // Serial.println(ax);
+    // Serial.print("ay = ");
+    // Serial.println(ay);
+    // Serial.print("az = ");
+    // Serial.println(az);
 
     //accelerometer angle
     float theta_a = atan(ay/ax)*180/3.14;
-    Serial.print("accelerometer angle: ");
-    Serial.println(theta_a);
+    // Serial.print("accelerometer angle: ");
+    // Serial.println(theta_a);
 
     IMU.readGyroscope(gx, gy, gz);
 
-    Serial.print("gx = ");
-    Serial.println(gx);
-    Serial.print("gy = ");
-    Serial.println(gy);
-    Serial.print("gz = ");
-    Serial.println(gz);
+    // Serial.print("gx = ");
+    // Serial.println(gx);
+    // Serial.print("gy = ");
+    // Serial.println(gy);
+    // Serial.print("gz = ");
+    // Serial.println(gz);
     // calculate gyroscope angle with previous angle measurement
     float theta_g = theta + gz*sampling_period/1000;
-    Serial.print("gyroscope angle: ");
-    Serial.println(theta_g);
+    // Serial.print("gyroscope angle: ");
+    // Serial.println(theta_g);
 
     //complementary filter
     theta = k*theta_g + (1-k)*(theta_a);
-    Serial.print("filtered angle: ");
-    Serial.println(theta);
+    // Serial.print("filtered angle: ");
+    // Serial.println(theta);
+
+    Serial.println(String(ax) + "," + String(ay) + "," + String(az));
   }
 
 }
