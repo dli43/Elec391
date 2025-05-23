d99a63a7be78785a5fc0ba70b67dd33f61983d49
Daniel <dlidaniel2004@gmail.com>
2025-05-23 16:36:07 -0700

    Create pwm test file

---
+++ pwm/pwm.ino
@@ -0,0 +1,21 @@
+
+int A1_MD = 5;       // Set the pin numbers for the h-bridge driver motor 
+int A2_MD = 4;   
+int B1_MD = 2;   
+int B2_MD = 3;
+int duty_cycle = 128;     // Set duty cycle value for PWM: 0 (always off), 255(always on)         
+
+
+void setup() {
+  pinMode(A1_MD, OUTPUT);     // Set the h-bridge driver pins as outputs
+  pinMode(A2_MD, OUTPUT);
+  pinMode(B1_MD, OUTPUT);
+  pinMode(B2_MD, OUTPUT);
+}
+
+void loop() {
+  analogWrite(A1_MD, duty_cycle);
+  analogWrite(A2_MD, duty_cycle);
+  analogWrite(B1_MD, duty_cycle);
+  analogWrite(B2_MD, duty_cycle);
+}
