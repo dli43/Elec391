71100608e24c857c757f645da75191ea1dc29648
Graham <gsmith13@student.ubc.ca>
2025-05-28 16:24:48 -0700

    task4

---
+++ proof-of-concept-demo/task4/task4.ino
@@ -0,0 +1,37 @@
+
+int A1_MD = 5;       // Set the pin numbers for the h-bridge driver motor 
+int A2_MD = 4;   
+int B1_MD = 2;   
+int B2_MD = 3;
+int duty_cycle = 195;     // Set duty cycle value for PWM: 0 (always on), 255(always off)         
+
+
+void setup() {
+  pinMode(A1_MD, OUTPUT);     // Set the h-bridge driver pins as outputs
+  pinMode(A2_MD, OUTPUT);
+  pinMode(B1_MD, OUTPUT);
+  pinMode(B2_MD, OUTPUT);
+
+  if(!IMU.begin()){
+    while(1)
+  }
+}
+
+void loop() {
+
+    //25% power for 5s
+    duty_cycle = 191;
+    digitalWrite(A1_MD, HIGH);
+    digitalWrite(B1_MD, duty_cycle);
+    analogWrite(A2_MD, duty_cycle);
+    analogWrite(B2_MD, HIGH);
+    delay(5000);
+
+    //75% power for 5s
+    duty_cycle = 63;
+    digitalWrite(A1_MD, HIGH);
+    digitalWrite(B1_MD, duty_cycle);
+    analogWrite(A2_MD, duty_cycle);
+    analogWrite(B2_MD, HIGH);
+    delay(5000);
+}
