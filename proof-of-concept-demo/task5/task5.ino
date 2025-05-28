6e457a2aa60ebe8b013a6963f8313ddf3b0d6ea2
Graham <gsmith13@student.ubc.ca>
2025-05-28 15:40:03 -0700

    angle stuff

--- proof-of-concept-demo/task5/task5.ino
+++ proof-of-concept-demo/task5/task5.ino
@@ -1,23 +1,87 @@
+#include "Arduino_BMI270_BMM150.h"
+
+const float pi = 3.14159;
+const float k = 0.5;
+const float cutoff_angle = 30.0;
+bool reference_angle_computed = false;
+bool new_filtered_angle = false;
+float ax,ay,az;
+float gx,gy,gz;
+float theta_a, theta_g, theta_k;
+unsigned long prev_time, present_time;
+float elapsed_time;
+char userInput;
 
 int A1_MD = 5;       // Set the pin numbers for the h-bridge driver motor 
 int A2_MD = 4;   
 int B1_MD = 2;   
 int B2_MD = 3;
-int duty_cycle = 255;     // Set duty cycle value for PWM: 0 (always off), 255(always on)         
-
+int duty_cycle = 0;
 
 void setup() {
+
   pinMode(A1_MD, OUTPUT);     // Set the h-bridge driver pins as outputs
   pinMode(A2_MD, OUTPUT);
   pinMode(B1_MD, OUTPUT);
   pinMode(B2_MD, OUTPUT);
+  
 
-  digitalWrite(A1_MD, HIGH);
-  analogWrite(A2_MD, duty_cycle);
-  digitalWrite(B1_MD, HIGH);
-  analogWrite(B2_MD, duty_cycle);
 }
 
 void loop() {
-  
+
+  if(IMU.accelerationAvailable()){
+    IMU.readAcceleration(ax, ay, az);       // Read acceleration values into variables
+    theta_a = (180/pi)*atan(ay/az);         // Compute new acceleration theta
+    
+    if(!reference_angle_computed){          // Assign reference values for gyroscope and filtered theta
+      prev_time = micros();                 // Mark time when acceleration is sampled
+      theta_g = theta_a;
+      theta_k = theta_a;
+      reference_angle_computed = true;
+    }
+    
+  }
+
+  if(IMU.gyroscopeAvailable()){
+    IMU.readGyroscope(gx,gy,gz);            // Read angular velocity values into variables 
+    
+    if(reference_angle_computed){
+      present_time = micros();          
+      elapsed_time = (float)(present_time - prev_time)/1e6;           // Calculate time that has passed since last sample read
+      prev_time = present_time;
+      theta_g = theta_g + (-gx)*elapsed_time;                         // Compute theta_g
+      theta_k = k*(theta_k + (-gx)*elapsed_time) + (1-k)*theta_a;     // Compute theta_k
+      new_filtered_angle = true;
+    }
+
+  }
+
+  if(new_filtered_angle){
+
+    //calculate pwm value based on absolute value of tilt angle
+    duty_cycle = 255 - min(int(255*abs(theta_k)/cutoff_angle), 255);
+    
+    //tilted forward
+    if(theta_k > 0){
+      digitalWrite(A1_MD, HIGH);
+      analogWrite(A2_MD, duty_cycle);
+      digitalWrite(B1_MD, HIGH);
+      analogWrite(B2_MD, duty_cycle);
+    }
+    //tilted backward
+    else if(theta_k > 0){
+      analogWrite(A1_MD, duty_cycle);
+      digitalWrite(A2_MD, LOW);
+      analogWrite(B1_MD, duty_cycle);
+      digitalWrite(B2_MD, HIGH);
+    }
+    //no tilt
+    else{
+      digitalWrite(A1_MD, LOW);
+      digitalWrite(A2_MD, LOW);
+      digitalWrite(B1_MD, LOW);
+      digitalWrite(B2_MD, LOW);
+    }
+  }
 }
