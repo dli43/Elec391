f80e466f98ae87cfe12a633dbe81be14f215da9d
Daniel <dlidaniel2004@gmail.com>
2025-06-10 16:58:16 -0700

    Update rotor speed arduino code

--- final-demo/rotor-speed-characteristic/AS5600_angular_speed/AS5600_angular_speed.ino
+++ final-demo/rotor-speed-characteristic/AS5600_angular_speed/AS5600_angular_speed.ino
@@ -16,10 +16,19 @@
 //  AS5600L as5600;   //  use default Wire
 AS5600 as5600;   //  use default Wire
 
+int A1_MD = 5;       // Set the pin numbers for the h-bridge driver motor 
+int A2_MD = 4;   
+int B1_MD = 2;   
+int B2_MD = 3;
+int duty_cycle;      // Set duty cycle value for PWM: 0 (always on), 255(always off)
+
+int as5600_select = 6;    // Select the address pin for the i2c multiplexer
+
 
 void setup()
 {
   while(!Serial);
+
   Serial.begin(115200);
   Serial.println(__FILE__);
   Serial.print("AS5600_LIB_VERSION: ");
@@ -27,9 +36,6 @@ void setup()
   Serial.println();
 
   Wire.begin();
-
-  as5600.begin(4);  //  set direction pin.
-  as5600.setDirection(AS5600_CLOCK_WISE);  //  default, just be explicit.
 
   Serial.println(as5600.getAddress());
 
@@ -39,17 +45,27 @@ void setup()
   Serial.print("Connect: ");
   Serial.println(b);
 
+  pinMode(A1_MD, OUTPUT);     // Set the h-bridge driver pins as outputs
+  pinMode(A2_MD, OUTPUT);
+  pinMode(B1_MD, OUTPUT);
+  pinMode(B2_MD, OUTPUT);
+
+  pinMode(as5600_select, OUTPUT);   // Set the i2c multiplexer select pin as output
+
   delay(1000);
 }
 
 
 void loop()
 {
-  //  Serial.print("\ta = ");
-  //  Serial.print(as5600.readAngle());
-  //  Serial.print("\tω = ");
-  Serial.println(as5600.getAngularSpeed());
-  delay(25);
+  duty_cycle = 191;     // 25% opposite wheel turn direction
+  digitalWrite(A1_MD, HIGH);
+  analogWrite(A2_MD, duty_cycle);
+  analogWrite(B1_MD, duty_cycle);
+  digitalWrite(B2_MD, HIGH);
+
+  Serial.println(as5600.getAngularSpeed(AS5600_MODE_RPM));
+  delay(50);
 }
 
 
