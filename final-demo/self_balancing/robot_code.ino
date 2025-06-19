7b03d166f907dc542120eb431e99b0d2d87a66bb
Graham <gsmith13@student.ubc.ca>
2025-06-19 01:01:39 -0700

    rotation correction fix

--- final-demo/self_balancing/robot_code.ino
+++ final-demo/self_balancing/robot_code.ino
@@ -8,8 +8,10 @@
   #define encoder_left 1
   #define encoder_right 0
   #define vel_buf_samples 50 // number of velocity measurements in sensor buffer
+  #define pos_buf_samples 4  // number of position measurements in sensor buffer
 
   #define print_interval 20
+  #define position_interval 20
   #define velocity_interval 5
 
   // Constants and flags
@@ -25,20 +27,40 @@
   bool compute_complementary_angle = false;
   
   // Velocity controller 
+  int vel_buf_tracker = 0;
   float encoder_velocities_left[vel_buf_samples];
   float encoder_velocities_right[vel_buf_samples];
+  float vel_kernel[vel_buf_samples];
+  float desired_left_velocity = 0;
+  float desired_right_velocity = 0;
   float vel_left = 0;
   float vel_right = 0;
-  float average_velocity = 0;
-  float offset_velocity = 0;
-  float desired_velocity = 0;
-  int vel_buf_tracker = 0;
-  float vel_avg_kernel[vel_buf_samples];
-  
-  float tau = 0.5; //time constant to correct velocity error
+  float correct_left_velocity = 0;
+  float correct_right_velocity = 0;
+  float correct_robot_velocity = 0;
+  float offset_wheel_velocity = 0;
+
+  float vel_tau = 0.5; //time constant to correct velocity error
   float desired_acceleration = 0;
   float desired_angle = 0;
   float max_desired_angle = 3;
+  float drive_speed = 0.3; //speed to drive robot at when given move commands
+  float reference_position = 0; // refrence where the robot starts
+
+  // Position controller
+  int pos_buf_tracker = 0;
+  long encoder_ticks_left = 0;
+  long encoder_ticks_right = 0;
+  float encoder_position_left[pos_buf_samples];
+  float encoder_position_right[pos_buf_samples];
+  float pos_kernel[pos_buf_samples];
+  float desired_position_left = 0;
+  float desired_position_right = 0;
+  float position_left = 0;
+  float position_right = 0;
+  float correct_position_left = 0;
+  float correct_position_right = 0;
+  float pos_tau = 1;
 
   // Bluetooth
   const char* BLE_name = "TEAM1-BLE-ROBOT";
@@ -67,12 +89,13 @@
   
   // Counters 
   int print_count = 1;
+  int position_count = 1;
   int velocity_count = 1;
   int angle_count = 1;
   
   
   // Tilt Angle controller
-  float kp = 3.2;        // PID values
+  float kp = 3.2;     // PID values
   float ki = 17;
   float kd = 0.3;
   float pid_p = 0;
@@ -93,35 +116,29 @@
   void setup() {
     Serial.begin(115200);       // Initialize serial communication
 
-    pinMode(A1_MD, OUTPUT);     // Set the h-bridge driver pins as outputs
-    pinMode(A2_MD, OUTPUT);
-    pinMode(B1_MD, OUTPUT);
-    pinMode(B2_MD, OUTPUT);
+    set_motordrivers();         // Set the h-bridge driver pins 
 
     if (!IMU.begin()) {     // Check if IMU module has initialized
       Serial.println("IMU failed");
       while (1);
     }
 
-    // Reset I2C multiplexer
-    pinMode(tca_reset, OUTPUT);
-    digitalWrite(tca_reset, LOW);
-    delay(25);
-    digitalWrite(tca_reset,HIGH);
-
+    reset_tca();    // Reset I2C multiplexer
+    
     Wire.begin();       // Initialize I2C
 
     pinMode(LED_BUILTIN, OUTPUT);
     initialize_BLE();
 
     //populate encoder arrays with zeroes
-    for(int i = 0; i < vel_buf_samples; i++){
-      encoder_velocities_left[i] = 0;
-      encoder_velocities_right[i] = 0;
-    }
+    initialize_buffers();
 
     // Initialize filter kernels
-    initialize_kernel(vel_avg_kernel, vel_buf_samples);
+    initialize_kernel(vel_kernel, vel_buf_samples);
+    initialize_kernel(pos_kernel, pos_buf_samples);
+
+    as5600_left.setZPosition(0);
+    as5600_right.setZPosition(0);
     
   }
 
@@ -149,16 +166,32 @@
       //update sensor buffers with most recent measurements
       update_sensor_buffers();
 
-      //calculate desired tilt
+      measure_wheel_ticks(); //find current position in wheel ticks
+      calculate_wheel_positions(); //update position buffers with current position in meters
+
+      //calculate desired position
+      if(position_count % position_interval == 0){
+        position_left = get_filtered_value(encoder_position_left, pos_kernel, pos_buf_samples, pos_buf_tracker);
+        position_right = get_filtered_value(encoder_position_right, pos_kernel, pos_buf_samples, pos_buf_tracker);
+        correct_position_left = desired_position_left - position_left;
+        correct_position_right = desired_position_right - position_right;
+        desired_left_velocity = correct_position_left/pos_tau;
+        desired_right_velocity = correct_position_right/pos_tau;
+      }
+
+      //calculate desired velocity and tilt
       if(velocity_count % velocity_interval == 0){ // Error velocity and tilt control angle adjusted every velocity_interval samples
-        vel_left = get_filtered_value(encoder_velocities_left, vel_avg_kernel, vel_buf_samples, vel_buf_tracker);
-        vel_right = get_filtered_value(encoder_velocities_right, vel_avg_kernel, vel_buf_samples, vel_buf_tracker);
-        average_velocity = (vel_left + vel_right)/2;
-        offset_velocity = abs(vel_left - vel_right)/2;
-        desired_acceleration = (desired_velocity-average_velocity)/tau;
+        vel_left = wheel_radius*get_filtered_value(encoder_velocities_left, vel_kernel, vel_buf_samples, vel_buf_tracker);
+        vel_right = wheel_radius*get_filtered_value(encoder_velocities_right, vel_kernel, vel_buf_samples, vel_buf_tracker);
+        correct_left_velocity = desired_left_velocity - vel_left;
+        correct_right_velocity = desired_right_velocity - vel_right;
+        correct_robot_velocity = (correct_left_velocity + correct_right_velocity)/2;
+        offset_wheel_velocity = abs(correct_left_velocity - correct_right_velocity)/(2*wheel_radius);
+        desired_acceleration = correct_robot_velocity/vel_tau;
         desired_angle = (180/pi)*atan(desired_acceleration/9.807);
         desired_angle = constrain(desired_angle, -max_desired_angle, max_desired_angle);
       }
+      
       calculate_pwm();
       drive_wheels();
 
@@ -167,7 +200,9 @@
       }
 
       print_count = (print_count + 1) % print_interval;
+      position_count = (position_count + 1) % position_interval;
       velocity_count = (velocity_count + 1) % velocity_interval;
+      pos_buf_tracker = (pos_buf_tracker + 1) % pos_buf_samples;
       vel_buf_tracker = (vel_buf_tracker + 1) % vel_buf_samples;
       compute_complementary_angle = false;
 
@@ -217,7 +252,6 @@
     //find correction angle
     float correction_angle = theta_k - standing_angle - desired_angle;
 
-    
     integral += correction_angle*elapsed_time;
     integral = constrain(integral, -integral_constraint, integral_constraint);
 
@@ -230,74 +264,60 @@
     float pid_left;
     float pid_right;
 
-    if(vel_left - vel_right > 0){
-      pid_left = pid - kphi/wheel_radius*offset_velocity;
-      pid_right = pid + kphi/wheel_radius*offset_velocity;
+    if(correct_left_velocity - correct_right_velocity > 0){
+      pid_left = pid + kphi*offset_wheel_velocity;
+      pid_right = pid - kphi*offset_wheel_velocity;
     }
-    else if(vel_right - vel_left > 0){
-      pid_left = pid + kphi/wheel_radius*offset_velocity;
-      pid_right = pid - kphi/wheel_radius*offset_velocity;
+    else if(correct_right_velocity - correct_left_velocity > 0){
+      pid_left = pid - kphi*offset_wheel_velocity;
+      pid_right = pid + kphi*offset_wheel_velocity;
     }
     else{
       pid_left = pid;
       pid_right = pid;
     }
 
-    if(pid_left > 0){
-      dutycycle_drive_left = int(pid_left+0.5);
-    }
-    else if(pid_left < 0){
-      dutycycle_drive_left = int(pid_left-0.5);
-    }
-    else{
-      dutycycle_drive_left = 0;
-    }
-
-    if(pid_right > 0){
-      dutycycle_drive_right = int(pid_right+0.5);
-    }
-    else if(pid_right < 0){
-      dutycycle_drive_right = int(pid_right-0.5);
-    }
-    else{
-      dutycycle_drive_right = 0;
-    }
+    dutycycle_drive_left = round_float(pid_left);
+    dutycycle_drive_right = round_float(pid_right);
 
     duty_cycle_left = min(abs(dutycycle_drive_left)+pwm_deadzone, 255);
     duty_cycle_right = min(abs(dutycycle_drive_right)+pwm_deadzone, 255);
   }
 
+  int round_float(float f){
+    if(f > 0){
+      return int(f+0.5);
+    }
+    else if(f < 0){
+      return int(f-0.5);
+    }
+    else{
+      return 0;
+    }
+  }
+
   void drive_wheels(){
 
-    //drive forward
-    if(dutycycle_drive_left > 0){
-      analogWrite(A1_MD, duty_cycle_left);
-      analogWrite(A2_MD, 0);
-    }
-    //drive backward
-    else if(dutycycle_drive_left < 0){
-      analogWrite(A1_MD, 0);
-      analogWrite(A2_MD, duty_cycle_left);
-    }
-    //no drive
-    else{
-      analogWrite(A1_MD, 0);
-      analogWrite(A2_MD, 0);
-    }
+    set_wheel_speed(A1_MD, A2_MD, dutycycle_drive_left, duty_cycle_left);
+    set_wheel_speed(B1_MD, B2_MD, dutycycle_drive_right, duty_cycle_right);
     
-    if(dutycycle_drive_right > 0){
-      analogWrite(B1_MD, duty_cycle_right);
-      analogWrite(B2_MD, 0);
+  }
+
+  void set_wheel_speed(int pin1, int pin2, int drive, int duty_cycle){
+    //drive forward
+    if(drive > 0){
+      analogWrite(pin1, duty_cycle);
+      analogWrite(pin2, 0);
     }
     //drive backward
-    else if(dutycycle_drive_right < 0){
-      analogWrite(B1_MD, 0);
-      analogWrite(B2_MD, duty_cycle_right);
+    else if(drive < 0){
+      analogWrite(pin1, 0);
+      analogWrite(pin2, duty_cycle);
     }
     //no drive
     else{
-      analogWrite(B1_MD, 0);
-      analogWrite(B2_MD, 0);
+      analogWrite(pin1, 0);
+      analogWrite(pin1, 0);
     }
   }
 
@@ -311,13 +331,17 @@
     Serial.print(" | D: ");
     Serial.print(pid_d);
     Serial.print("\n");
-    Serial.print("v_avg: ");
-    Serial.print(average_velocity);
+    Serial.print("v_error: ");
+    Serial.print(correct_robot_velocity);
     Serial.print(" desired acceleration: ");
     Serial.print(desired_acceleration);
     Serial.print(" desired angle: ");
     Serial.print(desired_angle);
     Serial.print("\n");
+    Serial.print("left pos: ");
+    Serial.println(position_left);
+    Serial.print("right pos: ");
+    Serial.println(position_right);
   }
 
   void update_pid(){
@@ -336,19 +360,11 @@
       Serial.println(integral_constraint, 4);
     }
 
-    //debugging
-    //Serial.println(input_str);
-
     int kpIndex = input_str.indexOf("kp");
     int kiIndex = input_str.indexOf("ki");
     int kdIndex = input_str.indexOf("kd");
     int saIndex = input_str.indexOf("sa");
     int icIndex = input_str.indexOf("ic");
-
-    //debugging
-    // Serial.println(kpIndex);
-    // Serial.println(kiIndex);
-    // Serial.println(kdIndex);
 
     if(kpIndex != -1 && kpIndex < input_str.length()){
       update_value('p', kpIndex+3);
@@ -487,37 +503,19 @@
   }
 
   void parse_BLE_string(String BLE_string){
-    BLE_string.toLowerCase();
+    // BLE_string.toLowerCase();
 
-    if(BLE_string.indexOf("forward") != -1){
-      desired_velocity = 0.10;
-    }
-    else if(BLE_string.indexOf("backward") != -1){
-      desired_velocity = -0.10;
-    }
-    else if(BLE_string.indexOf("stop") != -1){
-      desired_velocity = 0;
-    }
-  }
-
-  void update_average_velocity(){
-    //remove old measurements
-    average_velocity -= encoder_velocities_left[angle_count]/(2*vel_buf_samples);
-    average_velocity -= encoder_velocities_right[angle_count]/(2*vel_buf_samples);
-
-    //measure new velocities
-    tcaselect(encoder_left);
-    encoder_velocities_left[angle_count] = as5600_left.getAngularSpeed(AS5600_MODE_RADIANS);
-    encoder_velocities_left[angle_count] *= wheel_radius;
-    tcaselect(encoder_right);
-    encoder_velocities_right[angle_count] = as5600_right.getAngularSpeed(AS5600_MODE_RADIANS);
-    encoder_velocities_right[angle_count] *= wheel_radius;
-
-    //god only knows why the fuck i have to multiply by a negative here
-
-    //add new values to average
-    average_velocity += encoder_velocities_left[angle_count]/(2*vel_buf_samples);
-    average_velocity += encoder_velocities_right[angle_count]/(2*vel_buf_samples);
+    // if(BLE_string.indexOf("forward") != -1){
+    //   desired_velocity = drive_speed;
+    // }
+    // else if(BLE_string.indexOf("backward") != -1){
+    //   desired_velocity = -drive_speed;
+    // }
+    // else if(BLE_string.indexOf("stop") != -1){
+    //   desired_velocity = 0;
+    //   as5600_left.resetCumulativePosition(encoder_ticks_left);
+    //   as5600_right.resetCumulativePosition(encoder_ticks_righht);
+    // }
   }
 
   // Select channel on multiplexer
@@ -531,9 +529,9 @@
 
   void update_sensor_buffers(){
     tcaselect(encoder_left);
-    encoder_velocities_left[vel_buf_tracker] = as5600_left.getAngularSpeed(AS5600_MODE_RADIANS)*wheel_radius;
+    encoder_velocities_left[vel_buf_tracker] = as5600_left.getAngularSpeed(AS5600_MODE_RADIANS);
     tcaselect(encoder_right);
-    encoder_velocities_right[vel_buf_tracker] = as5600_right.getAngularSpeed(AS5600_MODE_RADIANS)*wheel_radius;
+    encoder_velocities_right[vel_buf_tracker] = as5600_right.getAngularSpeed(AS5600_MODE_RADIANS);
   }
 
 
@@ -557,5 +555,42 @@
 
     for(int i = 0; i < length; i++){
       kernel[i] /= sum;
+    }
+  }
+
+  void set_motordrivers(){
+    pinMode(A1_MD, OUTPUT);     
+    pinMode(A2_MD, OUTPUT);
+    pinMode(B1_MD, OUTPUT);
+    pinMode(B2_MD, OUTPUT);
+  }
+
+  void reset_tca(){
+    pinMode(tca_reset, OUTPUT);
+    digitalWrite(tca_reset, LOW);
+    delay(25);
+    digitalWrite(tca_reset,HIGH);
+  }
+
+  //measures distance traveled by each wheel in encoder ticks
+  void measure_wheel_ticks(){
+    encoder_ticks_left = as5600_left.getCumulativePosition();
+    encoder_ticks_right = as5600_right.getCumulativePosition();
+  }
+
+  //converts the encoder tick variables to meters
+  void calculate_wheel_positions(){
+    encoder_position_left[pos_buf_tracker] = encoder_ticks_left * (2*pi*wheel_radius)/4096;
+    encoder_position_right[pos_buf_tracker] = encoder_ticks_right * (2*pi*wheel_radius)/4096;
+  }
+
+  void initialize_buffers(){
+    for(int i = 0; i < vel_buf_samples; i++){
+      encoder_velocities_left[i] = 0;
+      encoder_velocities_right[i] = 0;
+    }
+    for(int i = 0; i < pos_buf_samples; i++){
+      encoder_position_left[i] = 0;
+      encoder_position_left[i] = 0;
     }
   }
